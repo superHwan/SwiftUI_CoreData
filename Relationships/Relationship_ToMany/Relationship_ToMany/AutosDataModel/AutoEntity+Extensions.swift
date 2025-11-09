936c492a9c99445abfe1565d00941c01dbe44c4c
superHwan <1529128636@qq.com>
2025-11-09 15:21:48 +0800

    handle nil and format data

---
+++ Relationships/Relationship_ToMany/Relationship_ToMany/AutosDataModel/AutoEntity+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  AutoEntity+Extensions.swift
+//  Relationships
+//
+//  Created by Sanny on 2025/11/5.
+//
+
+extension AutoEntity {
+    var viewModel: String {
+        model ?? "N/A"
+    }
+    
+    var viewYear: String {
+        year ?? "[Not Sure]"
+    }
+    
+    var viewManufacturer: String {
+        manufacturerEntity?.name ?? "N/A"
+    }
+}
