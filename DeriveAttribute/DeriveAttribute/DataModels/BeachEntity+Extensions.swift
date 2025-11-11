ca5050e38c70863860551dcdf8a2d2d784cdcefd
superHwan <1529128636@qq.com>
2025-11-11 16:26:18 +0800

    Create BeachEntity+Extensions.swift

---
+++ DeriveAttribute/DeriveAttribute/DataModels/BeachEntity+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  BeachEntity+Extensions.swift
+//  DeriveAttribute
+//
+//  Created by Sanny on 2025/11/9.
+//
+
+extension BeachEntity {
+    var viewCity: String {
+        city ?? ""
+    }
+    
+    var viewName: String {
+        name ?? "N/A"
+    }
+}
