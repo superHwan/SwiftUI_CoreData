3c1bde86d612605a0e494ce735a56e536d80afa3
superHwan <1529128636@qq.com>
2025-11-27 14:11:55 +0800

    Create ImageCache.swift

---
+++ UsingObservableObject/UsingObservableObject/Inactive/ImageCache.swift
@@ -0,0 +1,32 @@
+//
+//  ImageCache.swift
+//  UsingObservableObject
+//
+//  Created by Sanny on 2025/11/21.
+//
+import UIKit
+
+class ImageCache {
+    static let shared = ImageCache()
+    private let cache = NSCache<NSString, UIImage>()
+    
+    private init() {
+        cache.countLimit = 100 // 限制缓存数量
+    }
+    
+    func getImage(forKey key: String) -> UIImage? {
+        return cache.object(forKey: key as NSString)
+    }
+    
+    func setImage(_ image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key as NSString)
+    }
+    
+    func removeImage(forKey key: String) {
+        cache.removeObject(forKey: key as NSString)
+    }
+    
+    func clearCache() {
+        cache.removeAllObjects()
+    }
+}
