137af47f5e90a31368cb7cf2cdb184e6732867db
superHwan <1529128636@qq.com>
2025-11-28 19:45:36 +0800

    Create Book.swift

---
+++ Validation/Validation/BookDataModel/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  Validation
+//
+//  Created by Sanny on 2025/11/27.
+//
+import Foundation
+
+struct Book {
+    var name: String
+    var author: String
+    var rating: Double
+    var publicationDate: Date
+}
